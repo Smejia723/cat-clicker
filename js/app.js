015733f1a099d7ea75c312b8b9baf8946ac50e9c
Samuel <smejia723@gmail.com>
2017-12-29 16:14:35 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttribution = ko.observable('https://github.com/udacity/ud989-cat-clicker-ko-starter/blob/master/img/434164568_fea0ad4013_z.jpg');
+
+    this.title = ko.computed(function(){
+        var title;
+        var clicks = this.clickCount();
+        if (clicks < 20){
+            title = 'Newborn';
+        } else if (clicks < 30){
+            title = 'infant';
+        } else if (clicks < 50){
+            title = 'child';
+        } else if (clicks < 75){
+            title = 'teen';
+        } else if (clicks < 100){
+            title = 'adult';
+        }
+        return title;
+        console.log(title);
+    }, this);
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+}
+
+ko.applyBindings(new ViewModel());
