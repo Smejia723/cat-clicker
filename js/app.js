75d898f951b221b028580ba7f5787e57d9b6f213
Samuel <smejia723@gmail.com>
2018-01-02 14:51:18 -0500

    Add files via upload
    modify code to receive a database.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,9 @@
-var ViewModel = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://github.com/udacity/ud989-cat-clicker-ko-starter/blob/master/img/434164568_fea0ad4013_z.jpg');
-
-    self.nickname = ko.observableArray([
-        'kitty',
-        'orange',
-        'pupper',
-        'smol'
-    ]);
-
-    self.addNickname = function(){
-        self.nickname.push({ name: "New at " + new Date() });
-    };
-
-    self.removeNickname = function(){
-        self.nickname.remove(this);
-    }
+var Cat = function(data){
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    self.nickname = ko.observableArray(data.nickname);
 
     this.title = ko.computed(function(){
         var title;
@@ -36,9 +22,38 @@ var ViewModel = function() {
         return title;
         console.log(title);
     }, this);
+}
+
+
+var initialCats = [];
+
+
+var ViewModel = function() {
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable( new Cat({
+        clickCount: 0,
+        name: 'tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://github.com/udacity/ud989-cat-clicker-ko-starter/blob/master/img/434164568_fea0ad4013_z.jpg',
+        nickname:['kitty','orange','pupper','smol']
+    }) );
+    self.addNickname = function(data){
+        self.nickname.push({ name: "New at " + new Date(data.nickname) });
+    };
+
+    self.removeNickname = function(data){
+        self.nickname.remove(data.nickname);
+    };
 
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 }
 
